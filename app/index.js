f0fef56d3198ac1e4f4a1e5731f776db7b93ad4f
Kara Fulgum <karafulgum@gmail.com>
2017-03-09 15:28:10 -0600

    Dog card HTML

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
 import 'whatwg-fetch';
+
+// const toggle = document.querySelector('.nav__menu-toggle"');
+// const form = document.querySelector('.form');
+//
+// function showForm() {
+//   form.classList.toggle('.form--toggle');
+// }
+//
+// toggle.addEventListener('click', showForm);
